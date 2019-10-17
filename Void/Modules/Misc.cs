a60eb31615c0ecaf8c837cedc3cb43a3dc6ea9a4
rixis19 <thomascontey@gmail.com>
2019-10-17 13:44:00 -0400

    Initial Commit
    A discord bot.

---
+++ Void/Modules/Misc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Void.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Message by "+Context.User.Username);
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+        [Command("pick")]
+        public async Task PickOne([Remainder]string message)
+        {
+            string[] options = message.Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            Random r = new Random();
+            string selection = options[r.Next(0, options.Length)];
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Choice for " + Context.User.Username);
+            embed.WithDescription(selection);
+            embed.WithColor(new Color(255, 255, 0));
+            embed.WithThumbnailUrl(Context.User.GetAvatarUrl());
+
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+    }
+}
---
+++ Void/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Void
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key))
+            {
+                return alerts[key];
+            }
+            return "";
+        }
+    }
+}
