c0014e0eaed4397cde0d0452b5ac139b76680919
rixis19 <gatesofrixis@gmail.com>
2019-10-22 23:25:37 -0400

    Adding of Tomquotes and Remember/Recall

--- Void/Modules/Misc.cs
+++ Void/Modules/Misc.cs
@@ -7,15 +7,53 @@ namespace Void.Modules
 {
     public class Misc : ModuleBase<SocketCommandContext>
     {
+        Utilities util = new Utilities();
+
         [Command("echo")]
         public async Task Echo([Remainder]string message)
         {
             var embed = new EmbedBuilder();
-            embed.WithTitle("Message by "+Context.User.Username);
+            embed.WithTitle("Message by " + Context.User.Username);
             embed.WithDescription(message);
             embed.WithColor(new Color(0, 255, 0));
 
             await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+        [Command("tomquote")]
+        public async Task TomQuote()
+        {
+            await Context.Channel.SendMessageAsync(util.GetTomQuote());
+        }
+
+        [Command("hi")]
+        public async Task Hi()
+        {
+            await Context.Channel.SendMessageAsync("HULLOOOOOOOOO");
+        }
+
+        [Command("help")]
+        public async Task Help() //Inplemented
+        {
+         
+        }
+
+        public async Task Help([Remainder]string message) //Inplemented
+        {
+
+        }
+
+        [Command("remember")]
+        public async Task Remember([Remainder]string message)
+        {
+            util.SetRememberQuote(message);
+            await Context.Channel.SendMessageAsync("Remembered");
+        }
+
+        [Command("recall")]
+        public async Task Recall()
+        {
+            await Context.Channel.SendMessageAsync(util.GetQuote());
         }
 
         [Command("pick")]
--- Void/Utilities.cs
+++ Void/Utilities.cs
@@ -2,21 +2,45 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.IO;
+using System.Linq;
+using System.Collections;
 
 namespace Void
 {
-    class Utilities
+    public class Utilities
     {
         private static Dictionary<string, string> alerts;
+        private static Dictionary<string, string> quotes;
+        private const string memoryFolder = "SystemLang";
+        private const string memoryFile = "memory.json";
+        private static string selection;
 
-        static Utilities()
+        public static Dictionary<string, string> memories;
+
+        Random r = new Random();
+
+        public Utilities()
         {
             string json = File.ReadAllText("SystemLang/alerts.json");
             var data = JsonConvert.DeserializeObject<dynamic>(json);
             alerts = data.ToObject<Dictionary<string, string>>();
+            json = File.ReadAllText("SystemLang/tomquotes.json");
+            data = JsonConvert.DeserializeObject<dynamic>(json);
+            quotes = data.ToObject<Dictionary<string, string>>();
+
+            json = File.ReadAllText("SystemLang/memory.json");
+            data = JsonConvert.DeserializeObject<dynamic>(json);
+            memories = data.ToObject<Dictionary<string, string>>();
         }
 
-        public static string GetAlert(string key)
+        internal string GetQuote()
+        {
+            int intSelection = r.Next(memories.Count);
+            selection = memories[intSelection.ToString()];
+            return selection;
+        }
+
+        public string GetAlert(string key)
         {
             if (alerts.ContainsKey(key))
             {
@@ -24,5 +48,25 @@ namespace Void
             }
             return "";
         }
+
+        public void SetRememberQuote(string quote)
+        {
+            memories.Add((Convert.ToInt32(memories.Last().Key) + 1).ToString(), quote);
+
+            string json = JsonConvert.SerializeObject(memories);
+            File.WriteAllText(memoryFolder + "/" + memoryFile, json);
+
+            json = File.ReadAllText("SystemLang/memory.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            memories = data.ToObject<Dictionary<string, string>>();
+        }
+
+        public string GetTomQuote()
+        {
+            int intSelection = r.Next(quotes.Count);
+            selection = quotes[intSelection.ToString()];
+            return selection;
+        }
+        
     }
 }
